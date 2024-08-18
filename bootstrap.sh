a9ef227541fb419bb26b92c870ac6f039e1b1b25
daiwa.zou <103150487+daiwa-zou@users.noreply.github.com>
2024-08-18 15:44:19 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -179,10 +179,44 @@ set_zsh_as_default() {
     fi
 }
 
+# Function to install oh-my-zsh
+install_oh_my_zsh() {
+    local omz_dir="$HOME/.oh-my-zsh"
+
+    # Check if Oh My Zsh is already installed
+    if [ -d "$omz_dir" ]; then
+        echo "Oh My Zsh is already installed at $omz_dir."
+        return 0
+    fi
+
+    echo "Installing Oh My Zsh..."
+
+    # Attempt to install using curl or wget
+    if command -v curl > /dev/null 2>&1; then
+        if curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh --unattended | sh; then
+            echo "Oh My Zsh installed successfully."
+        else
+            echo "Failed to install Oh My Zsh using curl."
+            return 1
+        fi
+    elif command -v wget > /dev/null 2>&1; then
+        if wget --quiet https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh --unattended -O - | sh; then
+            echo "Oh My Zsh installed successfully."
+        else
+            echo "Failed to install Oh My Zsh using wget."
+            return 1
+        fi
+    else
+        echo "Error: Neither curl nor wget is installed. Please install one of them first."
+        return 1
+    fi
+}
+
 # Execute functions
 run_installers
 install_packages
 setup_gitconfig
+install_oh_my_zsh
 install_dotfiles
 if ! check_zsh_installed; then
     # Set zsh as the default shell
