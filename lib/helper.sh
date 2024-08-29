54eec33f51ab9b7a2fe29158fa576e928db071cc
daiwa.zou <103150487+daiwa-zou@users.noreply.github.com>
2024-08-29 16:25:57 -0400

    Update helper.sh

--- lib/helper.sh
+++ lib/helper.sh
@@ -1,1 +1,14 @@
+# Print formatted info message to stdout
+info() {
+    printf "\r  [ \033[00;34m..\033[0m ] %s\n" "$1"
+}
 
+success() {
+    printf "\r\033[2K  [ \033[00;32mOK\033[0m ] %s\n" "$1"
+}
+
+fail() {
+    printf "\r\033[2K  [\033[0;31mFAIL\033[0m] %s\n" "$1"
+    echo ''
+    exit 1
+}
