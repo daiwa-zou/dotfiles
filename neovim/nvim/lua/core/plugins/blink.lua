391d3460e9b23f03c6a8c077b227a4876340ca79
Daiwa Zou <daiwa.zou@outlook.com>
2025-04-05 10:47:18 -0400

    Add blink plugins

---
+++ neovim/nvim/lua/core/plugins/blink.lua
@@ -0,0 +1,79 @@
+return {
+  'saghen/blink.nvim',
+  build = 'cargo build --release', -- for delimiters
+  keys = {
+    -- chartoggle
+    {
+      '<C-;>',
+      function()
+        require('blink.chartoggle').toggle_char_eol ';'
+      end,
+      mode = { 'n', 'v' },
+      desc = 'Toggle ; at eol',
+    },
+    {
+      ',',
+      function()
+        require('blink.chartoggle').toggle_char_eol ','
+      end,
+      mode = { 'n', 'v' },
+      desc = 'Toggle , at eol',
+    },
+
+    -- tree
+    -- { '<C-e>', '<cmd>BlinkTree reveal<cr>', desc = 'Reveal current file in tree' },
+    -- { '<leader>E', '<cmd>BlinkTree toggle<cr>', desc = 'Reveal current file in tree' },
+    -- { '<leader>e', '<cmd>BlinkTree toggle-focus<cr>', desc = 'Toggle file tree focus' },
+  },
+  -- all modules handle lazy loading internally
+  lazy = false,
+  opts = {
+    chartoggle = { enabled = true },
+    indent = {
+      enabled = true,
+      visible = true,
+      blocked = {
+        buftypes = {},
+        filetypes = {},
+      },
+      static = {
+        enabled = true,
+        char = '▎',
+        priority = 1,
+        -- specify multiple highlights here for rainbow-style indent guides
+        -- highlights = { 'BlinkIndentRed', 'BlinkIndentOrange', 'BlinkIndentYellow', 'BlinkIndentGreen', 'BlinkIndentViolet', 'BlinkIndentCyan' },
+        highlights = { 'BlinkIndent' },
+      },
+      scope = {
+        enabled = true,
+        char = '▎',
+        priority = 1024,
+        -- set this to a single highlight, such as 'BlinkIndent' to disable rainbow-style indent guides
+        -- highlights = { 'BlinkIndent' },
+        highlights = {
+          'BlinkIndentRed',
+          'BlinkIndentYellow',
+          'BlinkIndentBlue',
+          'BlinkIndentOrange',
+          'BlinkIndentGreen',
+          'BlinkIndentViolet',
+          'BlinkIndentCyan',
+        },
+        underline = {
+          -- enable to show underlines on the line above the current scope
+          enabled = false,
+          highlights = {
+            'BlinkIndentRedUnderline',
+            'BlinkIndentYellowUnderline',
+            'BlinkIndentBlueUnderline',
+            'BlinkIndentOrangeUnderline',
+            'BlinkIndentGreenUnderline',
+            'BlinkIndentVioletUnderline',
+            'BlinkIndentCyanUnderline',
+          },
+        },
+      },
+    },
+    tree = { enabled = false },
+  },
+}
